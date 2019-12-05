8d9bc64d58809f8f18847e2790823524cb59546b
ilabstaff <ilabstaff@172.16.116.71>
2019-12-04 17:57:39 -0800

    worked on encapsulation

--- src/_01_introduction_to_encapsulation/EncapsulateTheData.java
+++ src/_01_introduction_to_encapsulation/EncapsulateTheData.java
@@ -15,20 +15,33 @@ package _01_introduction_to_encapsulation;
  * 
  * */
 
-
 public class EncapsulateTheData {
-	//1. Encapsulate the member variables.
-	//   Add restrictions to the setters according to the comment.
-	
-	//2. Create a new JUnit Test case and write tests to verify that 
-	//   the member variables' getters and setters are working
-	
-	private int itemsReceived; //must not be negative. All negative arguments get set to 0.
-	private float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
-	private String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
-	private Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
-	
+	// 1. Encapsulate the member variables.
+	// Add restrictions to the setters according to the comment.
+
+	// 2. Create a new JUnit Test case and write tests to verify that
+	// the member variables' getters and setters are working
+
+	private int itemsReceived; // must not be negative. All negative arguments get set to 0.
+	private float degreesTurned; // must be locked between 0.0 and 360.0 inclusive.
+	private String nomenclature = " "; // must not be set to a blank string. Blank Strings get set to a space
+	private Object memberObj; // must not be a String. If it is a String, set it equal to a new Object();
+
+	public int getitemsReceived() {
+		return this.itemsReceived;
+	}
+
+	public void setitemsReceived(int itemReceived) {
+		if (itemsReceived >= 0) {
+			this.itemsReceived = itemsReceived;
+		} else {
+			this.itemsReceived = 0;
+		}
+
+	}
+
+
 	public static void main(String[] args) {
-		
+
 	}
 }
